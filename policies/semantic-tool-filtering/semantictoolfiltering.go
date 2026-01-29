ea2a46e956fb3078e9871a886e7b76be5500c000
NaveenSandaruwan <naveensandaruwan101@gamil.com>
2026-01-29 08:29:36 +0530

    yyyyyyy

--- policies/semantic-tool-filtering/semantictoolfiltering.go
+++ policies/semantic-tool-filtering/semantictoolfiltering.go
@@ -710,7 +710,7 @@ func (p *SemanticToolFilteringPolicy) handleTextRequest(ctx *policy.RequestConte
 
 	// Generate embedding for user query
 	queryEmbedding, err := p.embeddingProvider.GetEmbedding(userQuery)
-	slog.Debug("xxxxxxxxxxxxxxxxxxxxxxx")
+	slog.Debug("yyyyyyyyyyyyyy")
 	if err != nil {
 		slog.Error("SemanticToolFiltering: Error generating query embedding", "error", err)
 		return p.buildErrorResponse("Error generating query embedding", err)
