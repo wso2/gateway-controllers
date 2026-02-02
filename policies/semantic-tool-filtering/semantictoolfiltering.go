993ec9203e7568fa1a0b45b40553283c25cb17ba
NaveenSandaruwan <naveensandaruwan101@gamil.com>
2026-02-02 10:38:29 +0530

    Revert "feat: Add cache hit cleaning mechanism. increase cache limits and implement bulk tool addition with optimized embedding processing."
    This reverts commit ef18a80d3acdae98a9c72223b82ec17d368f289a.

--- policies/semantic-tool-filtering/semantictoolfiltering.go
+++ policies/semantic-tool-filtering/semantictoolfiltering.go
@@ -802,8 +802,6 @@ func (p *SemanticToolFilteringPolicy) handleTextRequest(ctx *policy.RequestConte
 			}
 		}
 	}
-	slog.Debug("xxxxxxxxxxxxxxxxxxxxxxx")
-
 	slog.Debug("SemanticToolFiltering: Filtered tools (text mode)",
 		"originalCount", len(textTools),
 		"filteredCount", len(filteredToolNames),
